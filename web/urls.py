32d815df23e5a2856039e5e0915b874807ae8cb8
souzaoliv <chamaodev@gmail.com>
2023-10-06 16:56:39 -0300

    Mapeado urls inicial do app portifolio

--- web/urls.py
+++ web/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib import admin
+from django.urls.conf import include
+
+from portifolio.urls import urlpatterns as urls_portifolio
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(urls_portifolio)),
+
 ]
