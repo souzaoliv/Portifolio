9235e7cfa3e79b5e6ae2f06911c35a7fe94fe43c
souzaoliv <chamaodev@gmail.com>
2023-10-08 17:02:43 -0300

    Configurado index views como responsavel pelo formulario, adapitado o html para o formulario com recurso de feedback do envio do formulario

--- portifolio/views.py
+++ portifolio/views.py
@@ -1,8 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
-
+from . formulario import FormularioCadastro
 # Create your views here.
 def index(request):
-    return render(request, 'portifolio/index.html')
+    if request.method == "GET":
+        return render(request, 'portifolio/index.html')
+    if request.method == "POST":
+        formulario = FormularioCadastro(request.POST)
+        if formulario.is_valid():
+            formulario.save()
+            sucesso = "Seus dados foram enviados com sucesso."
+            return render(request, 'portifolio/index.html', {'sucesso': sucesso})
+        else:
+            erro = "Não foi possivel enviar o formulario, Retorne e reenvie por favor."
+        return render(request, 'portifolio/index.html', {'erro': erro})
+    else:
+        return render(request, 'portifolio/index.html')
+
+
+
+
+
+
 
