0702146b00b8859f07b796d024b572de6233550b
souzaoliv <chamaodev@gmail.com>
2023-10-10 15:46:46 -0300

    Atualizando templates padrão models e views

--- lista_de_tarefas/models.py
+++ lista_de_tarefas/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Tarefa(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField(max_length=500)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nome
--- lista_de_tarefas/views.py
+++ lista_de_tarefas/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.contrib.auth import login , logout
+from django.views.decorators.http import require_http_methods
+
+from .models import Tarefa
 
 # Create your views here.
 def index(request):
-    return HttpResponse("lsita de tarefas work")
+    return render(request, 'lista_de_tarefas/index.html')
+
+
+
+
+
 
