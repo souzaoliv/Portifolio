26fc0903d4a7c68a81036b4c0b3eb6acbefa607f
souzaoliv <chamaodev@gmail.com>
2023-10-11 15:19:20 -0300

    Criado funções base para cadastrar e logar

--- lista_de_tarefas/views.py
+++ lista_de_tarefas/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 from django.contrib.auth import login, logout
@@ -13,11 +13,51 @@ def index(request):
     return render(request, 'lista_de_tarefas/index.html')
 
 
+@require_http_methods(["POST", "GET"])
 def cadastrar(request):
-    ...
+    if request.method == "GET":
+        return render(request, 'lista_de_tarefas/cadastrar.html')
+    else:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        # se usuario ja existe sera redirecionado para a pagina de login
+        if User.objects.filter(username=username).exists():
+            messages.error(request, f'Usuário "{username}" já existe. Faca login!')
+            return redirect('logar')
+        else:
+            try:
+                user = User.objects.create_user(username=username, password=password)
+                user.save()
+                return redirect('exibir_tarefas')
+            except ValueError as e:
+                print(e)
+                messages.error(request, f'Erro ao criar usuário "{username}" . Tente novamente!')
+                return render(request,'lista_de_tarefas/cadastrar.html')
 
+@require_http_methods(["POST", "GET"])
 def logar(request):
-    ...
+    if request.method == "GET":
+        return render(request, 'lista_de_tarefas/logar.html')
+    else:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        # se usuario nao existe sera redirecionado para a pagina de cadastro
+        if user is None:
+            if User.objects.filter(username=username).exists():
+                messages.error(request, f'Senha incorreta para o usuário "{username}" . Tente novamente!')
+                return render(request,'lista_de_tarefas/logar.html')
+            else:
+                messages.error(request, f'Usuário "{username}" não existe. Faça o cadastro!')
+                return redirect('cadastrar')
+        else:
+            try:
+                login(request, user)
+                return redirect('exibir_tarefas')
+            except ValueError as e:
+                print(e)
+                messages.error(request, f'Erro ao logar usuário "{username}" . Tente novamente!')
+                return render(request, 'lista_de_tarefas/logar.html')
 
 def deslogar(request):
     ...
