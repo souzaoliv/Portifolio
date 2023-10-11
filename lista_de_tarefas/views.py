c6de88e7496ace917ad22961caa0d2e9cfcb896f
souzaoliv <chamaodev@gmail.com>
2023-10-11 14:44:20 -0300

    Atualizando views

--- lista_de_tarefas/views.py
+++ lista_de_tarefas/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
-from django.contrib.auth import login , logout
+from django.contrib.auth import login, logout
 from django.views.decorators.http import require_http_methods
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib import messages
 
 from .models import Tarefa
 
