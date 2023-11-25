5f74589b7f78122986cb33aa0c9dd95b3e165ae7
souzaoliv <chamaodev@gmail.com>
2023-11-25 00:29:30 -0300

    Committing local changes before rebase

--- web/settings/heroku.py
+++ web/settings/heroku.py
@@ -4,6 +4,17 @@ import django_on_heroku
 import os
 import psycopg2
 import dj_database_url
+<<<<<<< HEAD
+=======
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
+DATABASE_URL = os.environ['DATABASE_URL']
+
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+
+django_on_heroku.settings(locals())
+>>>>>>> 0d84c8f3c4e5bfcdc54352de9a661f3bbbbf9489
 
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
