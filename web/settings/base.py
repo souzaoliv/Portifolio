10edba940c1b6ee605845965305f0c082531dc2c
souzaoliv <chamaodev@gmail.com>
2023-11-25 00:41:29 -0300

    Atualizações nas configurações de arquivos estáticos e de mídia

--- web/settings/base.py
+++ web/settings/base.py
@@ -132,14 +132,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles', 'media')
-]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'media')
+# Configuração para arquivos de mídia
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 CKEDITOR_UPLOAD_PATH = "uploads/"
 CKEDITOR_CONFIGS = {
